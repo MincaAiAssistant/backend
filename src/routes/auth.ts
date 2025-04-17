173175dc87f5cefb2c08fca25cb9d81ae914f4af
Hoang Hung <95847972+hungmeomeo@users.noreply.github.com>
2025-04-17 11:13:15 +0700

    fileUploda

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { signUp } from '../helper/auth/signup'; // Ensure this path is correct
+import { signUp } from '../controllers/auth/signup'; // Ensure this path is correct
+import { login } from '../controllers/auth/login'; // Ensure this path is correct
 
 const routerAuth = express.Router();
 
 routerAuth.post('/signup', signUp);
+routerAuth.post('/login', login);
 
 export default routerAuth;
