3c24fe9ea4430c548a01138452b31fcba2071009
Hoang Hung <95847972+hungmeomeo@users.noreply.github.com>
2025-05-04 07:03:32 +0700

    remove type

--- src/controllers/chat/getAllChats.ts
+++ src/controllers/chat/getAllChats.ts
@@ -3,15 +3,11 @@ import { Chat } from '../../models/chat';
 
 export const getAllChats = async (req: any, res: any) => {
   const userid = (req as any).user.userid;
-  const { type } = req.query;
-  if (!type) {
-    return res.status(400).json({ error: 'Type is required' });
-  }
 
   try {
     const chats = (await sql`
         SELECT * FROM chats
-        WHERE userid = ${userid} AND type = ${type}
+        WHERE userid = ${userid} 
         ORDER BY updated_at DESC
       `) as Chat[];
     res.json({ chats });
