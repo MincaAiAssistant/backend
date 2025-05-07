28e44987a8520495977eae08ec64896c42e6ce09
Hoang Hung <95847972+hungmeomeo@users.noreply.github.com>
2025-05-07 17:16:39 +0700

    change get to post

--- src/routes/hubspot.route.ts
+++ src/routes/hubspot.route.ts
@@ -6,6 +6,6 @@ import { hubspotCallback } from '../controllers/auth/hubSpot/hubspotCallback';
 const routerHubspot = express.Router();
 
 routerHubspot.post('/store-tokens', authenticateToken, storeHubspotTokens);
-routerHubspot.get('/callback', authenticateToken, hubspotCallback);
+routerHubspot.post('/callback', authenticateToken, hubspotCallback);
 
 export default routerHubspot;
